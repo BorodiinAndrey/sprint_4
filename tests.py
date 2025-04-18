9ed3f9197487f5212d1b3b57c5c0817995683c30
Andrei Borodin <andrei.borodin@dcsportsoft.com>
2025-04-18 16:13:14 +0300

    Add unit tests for classes attribute's and add_new_book, set_book_genre, get_book_genre, get_books_genre method's

--- tests.py
+++ tests.py
@@ -17,8 +17,56 @@ class TestBooksCollector:
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
         # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        # словарь books_genre, который нам возвращает метод get_books_genre, имеет длину 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_name_longer_than_limit_add_zero_books(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Очень длинное название книги, 42 символа!!')
+
+        assert len(collector.get_books_genre()) == 0
+
+    def test_add_new_book_add_zero_value(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+
+        assert collector.get_books_genre()['Гордость и предубеждение и зомби'] == ''
+
+    def test_dict_books_genre_is_empty(self):
+
+        collector = BooksCollector()
+        assert len(collector.books_genre) == 0
+
+    def test_list_favorites_is_empty(self):
+
+        collector = BooksCollector()
+        assert len(collector.favorites) == 0
+
+    def test_list_genre_true(self):
+
+        collector = BooksCollector()
+        assert  collector.genre == ['Фантастика', 'Ужасы', 'Детективы', 'Мультфильмы', 'Комедии']
+
+    def test_list_genre_age_rating_true(self):
+        collector = BooksCollector()
+        assert collector.genre_age_rating == ['Ужасы', 'Детективы']
+
+    def test_set_book_genre_add_genre_true(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        collector.set_book_genre('Что делать, если ваш кот хочет вас убить', 'Ужасы')
+
+        assert collector.get_book_genre('Что делать, если ваш кот хочет вас убить') == 'Ужасы'
+
+    def test_set_book_genre_add_genre_false(self):
+
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.set_book_genre('Что делать, если ваш кот хочет вас убить', 'Ужасы')
+
+        assert collector.get_book_genre('Что делать, если ваш кот хочет вас убить') is None
