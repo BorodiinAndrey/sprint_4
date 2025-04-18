2b55ebf5dac932039b376921ff538096557678af
Andrei Borodin <andrei.borodin@dcsportsoft.com>
2025-04-18 18:00:18 +0300

    Add parametrize tests and change README.md

--- tests.py
+++ tests.py
@@ -1,33 +1,26 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    params = [
+            (['Гордость и предубеждение и зомби'], 1),
+            (['Гордость и предубеждение и зомби', 'Что делать, если ваш кот хочет вас убить'], 2),
+            ([''], 0),
+            (['Очень длинное название книги, 42 символа!!'], 0),
+            (['Очень длинное название книги, 40 символа'], 1),
+            (['Очень длинное название книги, 41 символ!'], 1)
+        ]
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_genre, который нам возвращает метод get_books_genre, имеет длину 2
-        assert len(collector.get_books_genre()) == 2
-
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
-    def test_add_new_book_name_longer_than_limit_add_zero_books(self):
+    @pytest.mark.parametrize('book_name, expected_count', params)
+    def test_add_new_book_limits(self, book_name, expected_count):
 
         collector = BooksCollector()
-        collector.add_new_book('Очень длинное название книги, 42 символа!!')
+        for name in book_name:
+            collector.add_new_book(name)
 
-        assert len(collector.get_books_genre()) == 0
+        assert len(collector.get_books_genre()) == expected_count
 
     def test_add_new_book_is_empty_value(self):
 
